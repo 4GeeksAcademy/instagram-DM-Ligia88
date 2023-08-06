43e0ab4671bd048787574bcf3b48f1f8c0128fc1
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-06 18:19:41 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -10,41 +10,48 @@ Base = declarative_base()
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    follower_id = Column(Integer, ForeignKey('user.id'))
-    follower = relationship("User", foreign_keys=[follower_id])
-    followed_id = Column(Integer, ForeignKey('user.id'))
-    followed = relationship("User", foreign_keys=[followed_id])
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="following")
+    user_to = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
     email = Column(String(100), nullable=False, unique=True)
+    
+    following = relationship("Follower", foreign_keys=[Follower.user_from_id], back_populates="user_from")
+    followers = relationship("Follower", foreign_keys=[Follower.user_to_id], back_populates="user_to")
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video'), nullable=False)
     url = Column(String(250))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", back_populates="media")
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    content = Column(String(500))
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship("User", back_populates="posts")
+    media = relationship("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    text = Column(String(250))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    comment = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author = relationship("User", back_populates="comments")
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
-    def to_dict(self):
-        return {}
+    post = relationship("Post", back_populates="comments")
 
 
     def to_dict(self):
