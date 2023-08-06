dd75b9db9b355da8ec41c4e66139dd1b5919b35a
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-06 17:24:51 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,47 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    follower = relationship("User", foreign_keys=[follower_id])
+    followed_id = Column(Integer, ForeignKey('user.id'))
+    followed = relationship("User", foreign_keys=[followed_id])
+
 class User(Base):
     __tablename__ = 'user'
-    ID = Column(Integer, primary_key=True)
-    username = Column(String(255), nullable=False)
-    firstname = Column(String(255), nullable=False)
-    lastname = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
 
 class Media(Base):
     __tablename__ = 'media'
-    ID = Column(Integer, primary_key=True)
-    type = Column(Enum('photo', 'video', 'other'), nullable=False)
-    url = Column(String(255), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.ID'))
-    post = relationship('Post')
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 class Post(Base):
     __tablename__ = 'post'
-    ID = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.ID'))
-    user = relationship('User')
-
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    content = Column(String(500))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 class Comment(Base):
     __tablename__ = 'comment'
-    ID = Column(Integer, primary_key=True)
-    text = Column(String, nullable=False)
-    author_id = Column(Integer, ForeignKey('user.ID'))
-    post_id = Column(Integer, ForeignKey('post.ID'))
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    def to_dict(self):
+        return {}
+
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
